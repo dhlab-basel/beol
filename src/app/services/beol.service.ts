07a6e361cb8fad0308f9690c6e51c8b965a14e27
Sepideh Alassi <>
2026-05-05 14:17:34 +0200

    change federation to wikidata to proxy for adding the user agent headers

--- src/app/services/beol.service.ts
+++ src/app/services/beol.service.ts
@@ -884,7 +884,7 @@ export class BeolService {
             BIND(URI(?endWikiIri_xsd) AS ?endWikiIri)
 
             OPTIONAL{
-                SERVICE <https://query.wikidata.org/sparql> {
+                SERVICE <https://wdqs-proxy/sparql> {
                     ?startWikiIri wdp:P625 ?coordinate_start.
 
                     ?coordinate_start wdps:P625 ?coordinate_node_start.
